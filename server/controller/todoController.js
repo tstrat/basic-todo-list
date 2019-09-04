1d21db0ee8a514a9d1ae486c649a79a74b8dfd3f
tstrat <tstrat@email.arizona.edu>
2019-09-04 12:19:11 -0700

    Reset for students when they fork/clone"

--- server/controller/todoController.js
+++ server/controller/todoController.js
@@ -1,27 +1,19 @@
 const todos = [{content: 'stuff', completed: true},{content: 'Help students', completed: false}];
 
+/*
+    EDIT THESE FUNCTIONS TO ENABLE THE TODO APP TO WORK
+*/
 module.exports = {
     getTodo: (req, res) => {
-        res.status(200).send(todos)
+
     },
     postTodo: (req, res) => {
-        const { content, completed } = req.body;
-        todos.push({
-            content: content || 'No Content',
-            completed: completed || false
-        });
-        res.status(200).send(todos)
+
     },
     updateTodo: (req, res) => {
-        const { completed } = req.body
-        const index = req.params.index;
-        todos[index].completed = completed;
-        res.status(200).send(todos)
+
     },
     deleteTodo: (req, res) => {
-        const index = req.params.index;
 
-        todos.splice(index, 1);
-        res.status(200).send(todos)
     }
 }
